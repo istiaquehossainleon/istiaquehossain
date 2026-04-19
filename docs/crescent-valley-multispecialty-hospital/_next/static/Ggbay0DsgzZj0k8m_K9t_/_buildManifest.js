0366f49566fb4e2bdbe3537a8bed4228caf11c26
istiaquehossainleon <istiaquehossain382@gmail.com>
2026-04-19 20:00:12 +0600

    istiaquehossain+averon

---
+++ docs/crescent-valley-multispecialty-hospital/_next/static/Ggbay0DsgzZj0k8m_K9t_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/crescent-valley-multispecialty-hospital//_next/:path+",
+        "destination": "/crescent-valley-multispecialty-hospital/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
